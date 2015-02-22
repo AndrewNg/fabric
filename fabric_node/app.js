c0082b9497d7c16b928d91bcb410c2c8ecd026d6
AndrewNg <andrew@chesscademy.com>
2015-02-22 06:09:59 -0800

    big changes, node app working well with stl export and import

--- fabric_node/app.js
+++ fabric_node/app.js
@@ -4,9 +4,17 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var Gun = require('gun');
+var fs = require('fs');
+var modelCount = 0;
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+
+// initialize gun
+var gun = Gun({
+    file: 'data.json'
+});
 
 var app = express();
 
@@ -24,12 +32,35 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 
+// endpoint for sending the final three.js object and STL file
+app.post('/export', function(req, res) {
+    console.log('writing to gun');
+    gun.set({
+        type: "STLFile",
+        posi: modelCount,
+        stl: req.body.stl,
+        json: req.body.json
+    }).key('model/' + modelCount);
+
+    fs.writeFile("./public/javascripts/" + modelCount + ".stl", req.body.stl, function(err) {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log("The file was saved!");
+    }
+    });
+
+    modelCount++;
+    res.end((modelCount - 1).toString());
+});
+
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
+
 
 /// error handlers
 
---
+++ fabric_node/public/javascripts/export.js
@@ -0,0 +1,60 @@
+// in this i'll export a basic triangle to STL and send it to the server for handling
+
+var data = {};
+var loadGeoFromJSON;
+var cubeJSON;
+var modelCount;
+
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+var cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+function render() {
+  requestAnimationFrame( render );
+  cube.rotation.x += 0.1;
+  cube.rotation.y += 0.1;
+  renderer.render( scene, camera );
+}
+
+render();
+
+function exportGeometry() {
+  var STLFile = generateSTL(geometry);
+  cubeJSON = cube.geometry.toJSON();
+  var formatCubeJSON = JSON.stringify( cubeJSON, null, '\t' );
+  formatCubeJSON = formatCubeJSON.replace( /[\n\t]+([\d\.e\-\[\]]+)/g, '$1' );
+
+  data.stl = STLFile;
+  data.json = formatCubeJSON;
+
+  $.ajax({
+    type: 'POST',
+    data: JSON.stringify(data),
+    contentType: 'application/json',
+    url: '/export',
+    success: function(data) {
+      console.log("success!");
+      modelCount = data;
+    },
+    error: function(xhr) {
+      console.log('Request Status: ' + xhr.status + ' Status Text: ' + xhr.statusText);
+    }
+  });
+}
+
+function loadFromSTL() {
+  var loader = new THREE.STLLoader();
+   loader.load( '/javascripts/' + '2' + '.stl', function ( geometry ) {
+     scene.add( new THREE.Mesh( geometry ) );
+   });
+}
